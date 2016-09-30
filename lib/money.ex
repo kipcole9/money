884c4efe7209e93823be13481546b96ff16e2152
Kip Cole <kipcole9@gmail.com>
2016-09-30 18:24:46 +0800

    Add moduledoc for Money

--- lib/money.ex
+++ lib/money.ex
@@ -2,6 +2,35 @@ defmodule Money do
   @moduledoc """
   Money implements a set of functions to store, retrieve and perform arithmetic
   on a %Money{} type that is composed of a currency code and a currency value.
+
+  Money is very opinionated in the interests of serving as a dependable library
+  that can underpin accounting and financial applications.  In its initial
+  release it can be expected that this contract may not be fully met.  But
+  thats the contract.
+
+  How is this opinion expressed:
+
+  1. Money must always have both a value and a currency code.
+
+  2. The currency code must always be valid.
+
+  3. Money arithmetic can only be performed when both operands are of the
+  same currency.
+
+  4. Money values are stored as a `Decimal`.
+
+  5. Money is serialised to the database as a custom Postgres type that includes
+  both the value and the currency.  Therefore for serialization Postgres is
+  assumed as the data store.
+
+  6. All arithmetic functions work in un-rounded fixed point.  No rounding
+  occurs automatically (unless expressly called out for a function).
+
+  7. Explicit rounding obeys the rounding rules for a given currency.  The
+  rounding rules are defined by the Unicode consortium in its CLDR
+  repository as implemented by the hex package `ex_cldr`.  These rules
+  define the number of fractional digits for a currency and the rounding
+  increment where appropriate.
   """
 
   @opaque t :: %Money{currency: atom, value: Decimal}
