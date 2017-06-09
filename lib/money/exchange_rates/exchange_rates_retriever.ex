5baa976aae72dd3372abe0ed68cb21f828a2a61b
Xavier Defrang <xavier.defrang@gmail.com>
2017-06-09 23:01:41 +0200

    Let configuration be set at runtime

--- lib/money/exchange_rates/exchange_rates_retriever.ex
+++ lib/money/exchange_rates/exchange_rates_retriever.ex
@@ -16,78 +16,83 @@ defmodule Money.ExchangeRates.Retriever do
   """
 
   use GenServer
+
+  require Logger
+
   @default_retrieval_interval 360_000
-  @retrieve_every Money.get_env(:exchange_rates_retrieve_every, @default_retrieval_interval)
-
   @default_callback_module Money.ExchangeRates.Callback
-  @callback_module Money.get_env(:callback_module, @default_callback_module)
 
-   def start_link(name) do
-     GenServer.start_link(__MODULE__, [], name: name)
-   end
+  defmodule Config do
+    defstruct retrieve_every: nil,
+              callback_module: nil,
+              log_levels: %{}
+  end
 
-   def init([]) do
-     require Logger
+  def start_link(name) do
+    state = %Config{
+      retrieve_every: Money.get_env(:exchange_rates_retrieve_every, @default_retrieval_interval),
+      callback_module: Money.get_env(:callback_module, @default_callback_module),
+      log_levels: %{
+        success: Money.get_env(:log_success, nil),
+        info: Money.get_env(:log_info, :warn),
+        failure: Money.get_env(:log_failure, :warn)
+      }
+    }
 
-     log(:info, "Starting exchange rate retrieval service")
-     log(:info, "Rates will be retrieved every #{div(@retrieve_every, 1000)} seconds.")
+    GenServer.start_link(__MODULE__, state, name: name)
+  end
+
+   def init(state) do
+     log(state, :info, "Starting exchange rate retrieval service")
+     log(state, :info, "Rates will be retrieved every #{div(state.retrieve_every, 1000)} seconds.")
+
      initialize_ets_table()
-     do_retrieve_rates()
-     schedule_work()
-     {:ok, []}
+
+     do_retrieve_rates(state)
+     schedule_work(state.retrieve_every)
+
+     {:ok, state}
    end
 
-   def handle_info(:latest, _state) do
-     state = do_retrieve_rates()
-     schedule_work()
+   def handle_info(:latest, state) do
+     do_retrieve_rates(state)
+     schedule_work(state.retrieve_every)
      {:noreply, state}
    end
 
-   def do_retrieve_rates do
-     case Money.ExchangeRates.get_latest_rates() do
-       {:ok, rates} ->
-         retrieved_at = DateTime.utc_now
-         :ets.insert(:exchange_rates, {:rates, rates})
-         :ets.insert(:exchange_rates, {:last_updated, retrieved_at})
-         apply(@callback_module, :rates_retrieved, [rates, retrieved_at])
-         log(:success, "Retrieved exchange rates successfully")
-       {:error, reason} ->
-         log(:failure, "Error retrieving exchange rates: #{inspect reason}")
-     end
-   end
+  def do_retrieve_rates(%{callback_module: callback_module} = state) do
+    case Money.ExchangeRates.get_latest_rates() do
+      {:ok, rates} ->
+        retrieved_at = DateTime.utc_now
+        store_rates(rates, retrieved_at)
+        apply(callback_module, :rates_retrieved, [rates, retrieved_at])
+        log(state, :success, "Retrieved exchange rates successfully")
+      {:error, reason} ->
+        log(state, :failure, "Error retrieving exchange rates: #{inspect reason}")
+    end
+    state
+  end
 
-   defp schedule_work do
-     Process.send_after(self(), :latest, @retrieve_every)
-   end
+  defp schedule_work(delay_ms) do
+    Process.send_after(self(), :latest, delay_ms)
+  end
 
-   defp initialize_ets_table do
-     :ets.new(:exchange_rates, [:named_table, read_concurrency: true])
-   end
+  defp initialize_ets_table do
+    :ets.new(:exchange_rates, [:named_table, read_concurrency: true])
+  end
 
-   @success_level Money.get_env(:log_success, nil)
-   defp log(:success, message) do
-     require Logger
+  defp store_rates(rates, retrieved_at) do
+    :ets.insert(:exchange_rates, {:rates, rates})
+    :ets.insert(:exchange_rates, {:last_updated, retrieved_at})
+  end
 
-     if not is_nil(@success_level) do
-       Logger.log(@success_level, message)
-     end
-   end
+  defp log(%{log_levels: log_levels}, key, message) do
+    case Map.get(log_levels, key) do
+      nil ->
+        nil
+      log_level ->
+        Logger.log(log_level, message)
+    end
+  end
 
-   @info_level Money.get_env(:log_info, :warn)
-   defp log(:info, message) do
-     require Logger
-
-     if not is_nil(@info_level) do
-       Logger.log(@info_level, message)
-     end
-   end
-
-   @failure_level Money.get_env(:log_failure, :warn)
-   defp log(:failure, message) do
-     require Logger
-
-     if not is_nil(@failure_level) do
-       Logger.log(@failure_level, message)
-     end
-   end
  end
