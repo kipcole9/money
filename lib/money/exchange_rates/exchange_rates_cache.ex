8e4945a5cee6471e40d436e249c5c7d5259cdd25
Kip Cole <kipcole9@gmail.com>
2018-02-10 19:26:27 +0800

    Refactor Cache to use internal get/1 and put/2

--- lib/money/exchange_rates/exchange_rates_cache.ex
+++ lib/money/exchange_rates/exchange_rates_cache.ex
@@ -16,34 +16,21 @@ defmodule Money.ExchangeRates.Cache do
   end
 
   def latest_rates do
-    try do
-      case :ets.lookup(@ets_table, :latest_rates) do
-        [{:latest_rates, rates}] -> {:ok, rates}
-        [] -> {:error, {Money.ExchangeRateError, "No exchange rates were found"}}
-      end
-    rescue
-      ArgumentError ->
-        Logger.error("Argument error getting exchange rates from ETS table")
-        {:error, {Money.ExchangeRateError, "No exchange rates are available"}}
+    case get(:latest_rates) do
+      nil ->
+        {:error, {Money.ExchangeRateError, "No exchange rates were found"}}
+      rates ->
+        rates
     end
   end
 
   def historic_rates(%Date{calendar: Calendar.ISO} = date) do
-    try do
-      case :ets.lookup(@ets_table, date) do
-        [{_date, rates}] ->
-          {:ok, rates}
-
-        [] ->
-          {:error,
-           {Money.ExchangeRateError, "No exchange rates for #{Date.to_string(date)} were found"}}
-      end
-    rescue
-      ArgumentError ->
-        Logger.error("Argument error getting historic exchange rates from ETS table")
-
+    case get(date) do
+      nil ->
         {:error,
-         {Money.ExchangeRateError, "No exchange rates for #{Date.to_string(date)} are available"}}
+         {Money.ExchangeRateError, "No exchange rates for #{Date.to_string(date)} were found"}}
+      rates ->
+        rates
     end
   end
 
@@ -53,29 +40,25 @@ defmodule Money.ExchangeRates.Cache do
   end
 
   def last_updated do
-    case :ets.lookup(@ets_table, :last_updated) do
-      [{:last_updated, timestamp}] ->
-        {:ok, timestamp}
-
-      [] ->
+    case get(:last_updated) do
+      nil ->
         Logger.error("Argument error getting last updated timestamp from ETS table")
         {:error, {Money.ExchangeRateError, "Last updated date is not known"}}
+      last_updated ->
+        last_updated
     end
   end
 
   def store_latest_rates(rates, retrieved_at) do
-    :ets.insert(@ets_table, {:latest_rates, rates})
-    :ets.insert(@ets_table, {:last_updated, retrieved_at})
+    put(:latest_rates, rates)
+    put(:last_updated, retrieved_at)
   rescue
     ArgumentError ->
       Logger.error("Failed to store latest rates")
   end
 
   def store_historic_rates(rates, date) do
-    :ets.insert(@ets_table, {date, rates})
-  rescue
-    ArgumentError ->
-      Logger.error("Failed to store historic rates for #{inspect(date)}")
+    put(date, rates)
   end
 
   def get(key) do
